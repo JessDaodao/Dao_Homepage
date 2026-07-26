fbed741aa10b8c3970833426c35b397218b77d0e
Csituka_D <694428809@qq.com>
2026-07-26 21:02:47 +0800

    init

---
+++ app/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="layout">
+    <div class="card">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fdf5e6;
+}
+
+.card {
+  width: 100%;
+  max-width: 720px;
+  padding: 48px 40px;
+  border-radius: 24px;
+  background-color: #ffffff;
+  box-shadow: none;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>欢迎</h1>
+    <p>这是一个基于 Nuxt 4 的个人主页项目。</p>
+  </div>
+</template>
